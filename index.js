695994ada467059b91e32417b1c288af0e99cdd0
gurneeshs <singhgurneesh2022@gmail.com>
2025-01-02 14:21:07 +0530

    dsfa

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors());
 app.use(cors({
-    origin: 'https://jovial-dusk-7af451.netlify.app/', // Specify your frontend origin
+    origin: 'https://jovial-dusk-7af451.netlify.app', // Specify your frontend origin
     methods: 'GET,POST,PUT,DELETE', // Specify the allowed methods
     credentials:true,
     allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
